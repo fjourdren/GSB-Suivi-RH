bb53692aa26ecfd1b1363895f239fdc89f78a9be
fjourdren <flavien.jourdren@gmail.com>
2018-05-21 13:32:49 +0200

    fix recherchelabel

--- GSB-Suivi-RH1/Recherche.Designer.vb
+++ GSB-Suivi-RH1/Recherche.Designer.vb
@@ -60,7 +60,7 @@ Partial Class Recherche
         Me.GroupBox1.Size = New System.Drawing.Size(508, 130)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.GroupBox1.Text = "Recherche"
         '
         'Button1
         '
